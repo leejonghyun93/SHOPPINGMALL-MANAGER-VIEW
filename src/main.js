2e45800e5ae70d596dc4a859bca7f08add61cdb7
djdjcoding <dongju12100@gmail.com>
2025-06-16 13:37:46 +0900

    로컬 변경사항 커밋

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import './assets/main.css'
+import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
-createApp(App).mount('#app')
-// 안녕안녕안녕안녕
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
